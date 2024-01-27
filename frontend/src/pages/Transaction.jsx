b24352d54a4b95517dee7ffb152b30e70ad81814
Ariel Lagata <35162013+Ariel09@users.noreply.github.com>
2024-01-27 10:32:25 +0800

    Validation for transaction

--- frontend/src/pages/Transaction.jsx
+++ frontend/src/pages/Transaction.jsx
@@ -13,6 +13,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 function Transaction() {
   const navigate = useNavigate();
   const {id} = useParams();
+  const [errors, setErrors] = useState('');
   const {token} = useStateContext();
   const [types, setTypes] = useState([]);
   const [categories, setCategories] = useState([]);
@@ -21,6 +22,12 @@ function Transaction() {
     type: '',
     category: '',
   });
+  const defaultError = {
+    amount: false,
+    type: false,
+    category: false
+  };
+  const [isError, setIsError] = useState(defaultError);
 
   if(id){
     useEffect(() => {
@@ -45,11 +52,20 @@ function Transaction() {
   }
 
   const handleChange = (e) => {
-
     dispatch({
       type: e.target.name,
       payload: e.target.value
     })
+    if(e.target.name === 'amount'){
+      if(!e.target.value){
+        setErrors('Invalid Input');
+        setIsError({...isError, [e.target.name] : true})
+        return;
+      }
+      setIsError(defaultError);
+      setErrors('');
+    }
+
   }
 
   const handleTypeChange = async (e) =>{
@@ -61,7 +77,35 @@ function Transaction() {
     await getCategory(e.target.value);
   }
 
+  const checkError = () => {
+    let error = {...isError}
+    let check = false
+    if(!transaction.type){
+      error = {...error, type: true};
+      setErrors('Field is Required');
+      check = true;
+    }
+
+    if(!transaction.amount){
+      error = {...error, amount : true};
+      setErrors('Field is Required');
+      check = true;
+    }
+    if(!transaction.category){
+      error = {...error, category : true};
+      setErrors('Field is Required');
+      check = true;
+    }
+    setIsError(error);
+    return check;
+  }
+
   const handleSubmit = async () => {
+    if(checkError() || errors){
+      console.log('Nag return')
+      return;
+    }
+    console.log('Di nag return')
     try {
       let method;
       let url;
@@ -105,7 +149,7 @@ function Transaction() {
       }
       
     } catch (error) {
-        console.error('Error: ', error.message)
+      console.error('Error: ', error.message)
     }
   }
 
@@ -130,7 +174,7 @@ function Transaction() {
           display: 'flex',
           placeContent: 'center',
         }}>
-            <Typography variant='h3'>Transaction</Typography>
+          <Typography variant='h3'>Transaction</Typography>
         </Box>
         <Box
           component="form"
@@ -140,13 +184,13 @@ function Transaction() {
           noValidate
           autoComplete="off"
         >
-          <TextField onChange={handleChange} id="amount" name='amount' label="Amount" variant="outlined" value={transaction.amount} type='number' />
-          <TextField onChange={handleTypeChange} id="type" name='type' label="Type" variant="outlined" value={transaction.type} select >
+          <TextField required error={isError.amount} helperText={isError.amount ? errors : null} onChange={handleChange} id="amount" name='amount' label="Amount" variant="outlined" value={transaction.amount} type='number' />
+          <TextField required error={isError.type} helperText={isError.type ? errors : null} onChange={handleTypeChange} id="type" name='type' label="Type" variant="outlined" value={transaction.type} select >
             {types && types.map(type => (
               <MenuItem key={type._id} value={type._id}>{type.type}</MenuItem>
             ))}
           </TextField>
-          <TextField onChange={handleChange} id="category" name='category' label="Category" variant="outlined" value={transaction.category} select >
+          <TextField required error={isError.category} helperText={isError.category ? errors : null} onChange={handleChange} id="category" name='category' label="Category" variant="outlined" value={transaction.category} select >
             {categories && categories.map(category => (
               <MenuItem key={category._id} value={category._id}>{category.category}</MenuItem>
             ))}
