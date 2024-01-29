f11e846c6618591f2ff637567da6a3dde169753e
Ariel Lagata <35162013+Ariel09@users.noreply.github.com>
2024-01-29 09:30:11 +0800

    gumana kana

--- frontend/src/layouts/DefaultLayout.jsx
+++ frontend/src/layouts/DefaultLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import { useStateContext } from "../contexts/ContextProvider";
 import { Navigate } from "react-router-dom";
-import NavBar from "../pages/NavBar";
+import NavBar from "../pages/Navbar";
 
 function DefaulLayout() {
   const {token} = useStateContext();
